de0d2eca80e0a7632254e6412a8a0f8edfbae70b
kykruz <k.kruzan@yahoo.com>
2024-11-23 15:23:47 -0600

    UserPosts model setup continued

--- Models/UserPosts.cs
+++ Models/UserPosts.cs
@@ -4,5 +4,13 @@ namespace WoWProject.Models;
 
 public class UserPosts
 {
+    public int Id {get; set;}
+
+    public string? Title {get; set;}
+
+    public string? Username {get; set;}
+
+    public string? Body {get; set;}
+
 
 }
